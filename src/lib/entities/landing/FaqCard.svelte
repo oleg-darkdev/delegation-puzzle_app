b7fca06578c59ce79f02747c8d81294b78ce087d
DarkDev <darkdev_brand@riseup.net>
2024-10-21 03:11:42 +0300

    alfa v.

---
+++ src/lib/entities/landing/FaqCard.svelte
@@ -0,0 +1,37 @@
+
+
+<script lang="ts">
+    // import {  } from '$shared';
+    // import {  } from '$widgets';
+    // import {  } from '$entities'
+  export let faqData;
+</script>
+
+<div  class="accordion w-dropdown">
+  <div class="accordion-toggle w-dropdown-toggle">
+    <div class="accordion-icon">
+      <div class="accordion-lottie"></div>
+    </div>
+    <div class="h5-heading flex flex-row">
+      <h5>{faqData.q}</h5>
+
+      <div class="w-10 h-10">
+
+      </div>
+
+    </div>
+  </div>
+  <nav class="accordion-list w-dropdown-list">
+    <div class="accordion-content">
+      {#each faqData.a as answer}
+        <p class="txt-on-white">
+          {answer}
+        </p>
+      {/each}
+    </div>
+  </nav>
+</div>
+
+<style lang="postcss">
+
+</style>
