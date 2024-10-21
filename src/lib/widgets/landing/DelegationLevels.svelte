d88c4798be8ceb5aeec9540220e729619ff464af
DarkDev <darkdev_brand@riseup.net>
2024-10-21 18:42:30 +0300

    upd content

--- src/lib/widgets/landing/DelegationLevels.svelte
+++ src/lib/widgets/landing/DelegationLevels.svelte
@@ -8,39 +8,41 @@
 
 
 <section class="section-delegation-levels bg-black wf-section">
+  <a name="about"></a>
 
   <div class="container">
    <div class="w-layout-grid grid-row-30 _50w">
-      <div class="overline">Lorem Ipsum has been</div>
+      <div class="overline">Poziomy delegowania</div>
       <h3 class="h3-heading">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</h3>
       <p class="paragraph txt-on-dark">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     </div>
     <div class="process-wrap">
       <div class="max-w-2xl">
         <div class="process-block ">
-          <div class="txt-on-dark mt-10 mt-0-mob">0</div>
-          <h4 class="h4-heading">Lorem Ipsum has been </h4>
+          <div class="txt-on-dark mt-10 mt-0-mob"></div>
+          <h4 class="h4-heading">Wstępne przygotowanie</h4>
           <div class="w-layout-grid grid-row-20 mt-10 mt-0-mob grid-left-al">
-            <p class="txt-on-dark">Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
-            <a href="/" class="btn w-button">Start here</a>
+            <p class="txt-on-dark">1. Kup lub wydrukuj zestaw gry planszowej.</p>
+            <p class="txt-on-dark">2. Zaloguj się do swojego konta w aplikacji.</p>
+
+            <a href="/app" class="btn w-button">Rozpocząć delegowanie</a>
           </div>
         </div>
 
-<div class="flex flex-row pt-20">
-    <img src="/images/delegation_levels.svg" loading="lazy" alt="" class="mt-[40px] h-screen w-20 process-illustr-1">
+        <div class="flex flex-row pt-20">
+          <img src="/images/delegation_levels.svg" loading="lazy" alt="" class=" h-screen w-32 delegation-levels-img">
 
-        <div class="w-layout-grid grid-row-100 grid-row-30-mob rel-z-2 max-w-2xl">
-      {#each delegationLevels as level}
-      <div class="process-block">
-        <div class="txt-on-dark mt-10 mt-0-mob">0{level.id}</div>
-        <h3 class="h4-heading">{level.title}</h3>
-        <p class="txt-on-dark">{level.shortDesc}</p>
-      </div>
-      {/each}
-      </div>
-      </div>
-
-
+          <div class="w-layout-grid grid-row-100 grid-row-30-mob rel-z-2 max-w-2xl">
+            {#each delegationLevels as level}
+              <div class="process-block lg:max-w-2xl ">
+                <div class="txt-on-dark mt-10 mt-0-mob">0{level.id}</div>
+                <h3 class="h4-heading"> <span class='{level.textColor}'>{level.title}</span> <img src="/images/{level.icon}" class='h-6 w-auto mb-1' alt=""></h3>
+                <p class="txt-on-dark">{level.subTitle}</p>
+                <!--  {level.shortDesc}  -->
+              </div>
+            {/each}
+          </div>
+        </div>
       </div>
     </div>
   </div>
