f9c4420d6342e51e06ff0c3651f579ae54c6508e
DarkDev <darkdev_brand@riseup.net>
2024-10-21 18:41:38 +0300

    upld img`s

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { SEO } from '$shared';
+  // Free_desc,
+	import { Footer, Header, About, Benefits, Faq, Price, OtherBoardgames, TargetAudience, DelegationLevels,  BigHero, SmallHero,  } from '$widgets';
+  // import {  } from ''
+
+  export let data;
+
+
+</script>
+
+
+<SEO title="Home"/>
+
+
+<style lang="postcss">
+
+</style>
