39b21ebda60c49e2e1af770234ff08548eb6856b
DarkDev <darkdev_brand@riseup.net>
2024-10-29 00:48:35 +0200

    del daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,22 +4,22 @@ module.exports = {
 		extends: {},
 		fontFamily: {}
 	},
-	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	plugins: [require('@tailwindcss/typography')],
 	// daisyUI config (optional)
-	daisyui: {
-		themes: [
-			{
-				mytheme: {
-					primary: '#661AE6',
-					secondary: '#D926AA',
-					accent: '#5AB034', // eco
-					neutral: '#191D24',
-					info: '#3ABFF8',
-					success: '#36D399',
-					warning: '#FBBD23',
-					error: '#F87272'
-				}
-			}
-		]
-	}
+	// daisyui: {
+	// 	themes: [
+	// 		{
+	// 			mytheme: {
+	// 				primary: '#661AE6',
+	// 				secondary: '#D926AA',
+	// 				accent: '#5AB034', // eco
+	// 				neutral: '#191D24',
+	// 				info: '#3ABFF8',
+	// 				success: '#36D399',
+	// 				warning: '#FBBD23',
+	// 				error: '#F87272'
+	// 			}
+	// 		}
+	// 	]
+	// }
 };
