5a11794d6588796afe60119f7494c773e3396727
DarkDev <darkdev_brand@riseup.net>
2024-11-03 19:03:35 +0200

    add animations

--- src/lib/widgets/app/InstructionScreenApp.svelte
+++ src/lib/widgets/app/InstructionScreenApp.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { blur, crossfade, draw, fade, fly, scale, slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
 	import { delegationLevels } from '$shared';
 	// import {  } from '$widgets';
 	import { DelegationsList, DelegationLevelCardApp } from '$entities';
@@ -7,8 +10,7 @@
 
 {#if $stepInstruction < 10}
 	{#if $stepInstruction == 1}
-		<!-- <section> -->
-		<div class="max-w-2xl mt-20 px-4">
+		<div transition:blur class="max-w-2xl mt-20 px-4">
 			<div class="overline">Poziomy delegowania</div>
 
 			<h3 class="h3-heading mb-20">7 poziomów delegowania to model symetryczny.</h3>
@@ -30,28 +32,110 @@
 				menedżera oraz między jednostkami lub zespołami w sposób peer-to-peer.
 			</p>
 		</div>
-
-		<!-- </section> -->
 	{:else if $stepInstruction == 2}
-		<div class="overflow-y-scroll overflow-x-hidden">
-			
-
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
 			<DelegationsList {delegationLevels} />
 		</div>
 	{:else if $stepInstruction == 3}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[0]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class=""
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[0]} />
+		</div>
 	{:else if $stepInstruction == 4}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[1]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[1]} />
+		</div>
 	{:else if $stepInstruction == 5}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[2]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[2]} />
+		</div>
 	{:else if $stepInstruction == 6}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[3]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[3]} />
+		</div>
 	{:else if $stepInstruction == 7}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[4]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[4]} />
+		</div>
 	{:else if $stepInstruction == 8}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[5]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[5]} />
+		</div>
 	{:else if $stepInstruction == 9}
-		<DelegationLevelCardApp delegationLevel={delegationLevels[6]} />
+		<div
+			transition:blur={{
+				delay: 100,
+				duration: 1000,
+				easing: quintOut,
+				inverse: true,
+				radius: 5
+			}}
+			class="overflow-y-scroll overflow-x-hidden"
+		>
+			<DelegationLevelCardApp delegationLevel={delegationLevels[6]} />
+		</div>
 	{/if}
 {:else if $stepInstruction == 10}
 	<h2>end wscren</h2>
