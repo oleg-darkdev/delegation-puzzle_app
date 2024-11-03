6bb1598358cfcc05dad0a4de69e5a80732c73e0f
DarkDev <darkdev_brand@riseup.net>
2024-11-03 15:54:48 +0200

    alfa v.

---
+++ src/lib/shared/ui/BackMenuBtn.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	// import {  } from '$shared';
+	// import {  } from '$widgets';
+	import { selectedMenu } from '$stores/app';
+</script>
+
+<button on:click={() => ($selectedMenu = 0)} class=" btn w-button"><span class="hidden sm:inline">Menu</span> <span class="inline sm:hidden">=</span> </button>
+
+<style lang="postcss">
+</style>
