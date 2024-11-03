87c933bd2f73b48fd6c00a2b3717b539e62be615
DarkDev <darkdev_brand@riseup.net>
2024-11-03 15:52:51 +0200

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -2,7 +2,7 @@
 import SEO from './ui/SEO.svelte';
 import NextStepBtn from './ui/NextStepBtn.svelte';
 import PrevStepBtn from './ui/PrevStepBtn.svelte';
-// import ComponenTemplate from './data/ComponenTemplate';
+import BackMenuBtn from './ui/BackMenuBtn.svelte';
 
 // data
 import faq from './data/faq';
@@ -13,4 +13,4 @@ import delegationLevels from './data/delegationLevels';
 // utils
 // import { fromBucket, from, auth, storage, supabaseClient } from './utils/supaClient';
 
-export { SEO, faq, navigation, delegationLevels, NextStepBtn, PrevStepBtn };
+export { SEO, faq, navigation, delegationLevels, BackMenuBtn, NextStepBtn, PrevStepBtn };
