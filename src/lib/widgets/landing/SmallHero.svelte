2f015453cbd8441dc9f3d4a99771458b632e5a27
DarkDev <darkdev_brand@riseup.net>
2024-11-03 20:37:25 +0200

    fix layout

--- src/lib/widgets/landing/SmallHero.svelte
+++ src/lib/widgets/landing/SmallHero.svelte
@@ -8,31 +8,31 @@
 	<div class="pg-hero-mob-pb">
 		<div class="container">
 			<div class="_50w rel-z-2">
-				<div class="grid-row-30">
+				<div class=" py-10">
 					<h1 class="txt-white">Rozłóż delegowanie zadań - jak puzzle.</h1>
 					<div class="_70w">
 						<p class="paragraph txt-on-dark mb-1">
 							Wykorzystaj <span class="txt-col-yellow">Delegation Puzzle</span>, aby wyjaśnić, kto
 							jest odpowiedzialny za co i na jakim poziomie.
 						</p>
-						<p class="paragraph txt-on-dark">
+						<!-- <p class="paragraph txt-on-dark">
 							Jest to edukacyjna gra planszowa, której możesz użyć, aby zacząć praktykować
 							zaangażowanie pracowników poprzez kontrolowaną samoorganizację i podejmowania decyzji.
-						</p>
+						</p> -->
 					</div>
 				</div>
 
-				<!-- <div class="abs-wrap flex-hor-all-cent">
+				<div class="abs-wrap flex-hor-all-cent">
 					<div class="abs-container flex-hor-all-cent">
 						<div class="service-cta-wrap">
 							<img src="/images/footer_doodle_arrow.svg" loading="lazy" alt="" class="hero-arrow" />
-							<a href="/" class="yellow-cta service-hero w-inline-block">
+							<button class="yellow-cta service-hero w-inline-block">
 								<img src="/images/cta.svg" loading="lazy" alt="" class="ye-cta-icon" />
-								<div class="btn-txt">Lorem Ipsum <br /> industry</div>
-							</a>
+								<div class="btn-txt">Początek  <br /> gry</div>
+							</button>
 						</div>
 					</div>
-				</div> -->
+				</div>
 			</div>
 		</div>
 	</div>
