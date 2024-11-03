28ecaf4446b3d2998b3aadfb422a54c9c46f8a3b
DarkDev <darkdev_brand@riseup.net>
2024-11-03 14:49:11 +0200

    beta v.

---
+++ src/lib/shared/ui/NextStepBtn.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	// import {  } from '$shared';
+	// import {  } from '$widgets';
+	const nextStep = () => step++;
+
+	export let step, text;
+</script>
+
+<button on:click={nextStep} class="btn w-button">{text}</button>
---
+++ src/lib/shared/ui/PrevStepBtn.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	// import {  } from '$shared';
+	// import {  } from '$widgets';
+	const prevStep = () => step--;
+
+	export let step, text;
+</script>
+
+<button on:click={prevStep} class="btn btn-black w-button">{text}</button>
